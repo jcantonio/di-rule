0ffa5177766b5469e402daea4f5e96deb68a8e2f
Jean-Claude Antonio <jcantonio@alineo.com>
2018-01-18 21:37:08 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # di-rule
-Go, Golang Rule Engine
+A Go Rules Engine
+
+Project from a Golang learner. Please give feedbacks. 
+
+Create Rules based on Conditions (Condition) which can be
+Logical Conditions (LogicalCondition) or Value Comparator Conditions (ComparatorCondition)
+
+Value Comparator Conditions are Value comparators with 
+	Path     string path to json element
+	Operator string 
+	Value    string¡bool¡numeric 
+
+Input json entity
+
+```bash
+go get -u github.com/jcantonio/di-rule
+```
+
+# Roadmap
+string comparator: equal        - DONE
+logical comparators: And        - DONE
+logical comparators: Or         - DONE
+string comparator: regex        - DONE
+nil comparator
+number comparator: equal
+number comparator: greater
+number comparator: less
+REST : Add Rule
---
+++ rule.go
@@ -0,0 +1,139 @@
+package dirule
+
+import (
+	"errors"
+	"regexp"
+
+	"github.com/tidwall/gjson"
+)
+
+const (
+	// And is the Logical Operator And
+	And = "and"
+	// Or is the Logical Operator Or
+	Or = "or"
+	//Equal is the Equal value comparator
+	Equal = "eq"
+	//Regexp is the Equal value comparator
+	Regexp = "regexp"
+	// Greater is the Greater value comparator
+	Greater = ">"
+	// Lesser is the Lesser value comparator
+	Lesser = "<"
+	// Nil is the Greater value comparator
+	Nil = "nil"
+)
+
+/*
+Condition needs to be implemented
+*/
+type Condition interface {
+	IsMet(currentEntityJSON *string) (bool, error)
+	//	IsMetWhenChanged(currentEntityJSON, previousEntityJSON string) (bool, error)
+}
+
+/*
+Rule compare
+*/
+type Rule struct {
+	ID        string
+	Condition Condition
+	Action    string
+}
+
+/*
+ComparatorCondition compare
+*/
+type ComparatorCondition struct {
+	Path     string
+	Operator string
+	Value    string
+}
+
+/*
+IsMet verifies whether the condition is met
+*/
+func (comparator *ComparatorCondition) IsMet(currentEntityJSON *string) (bool, error) {
+	result := gjson.Get(*currentEntityJSON, comparator.Path)
+	switch result.Type {
+	case gjson.Null:
+		return false, errors.New("Not implemented")
+	case gjson.True:
+		return false, errors.New("Not implemented")
+	case gjson.False:
+		return false, errors.New("Not implemented")
+	case gjson.JSON:
+		return false, errors.New("Not implemented")
+	case gjson.String:
+		return compareString(comparator, result.Str)
+	case gjson.Number:
+		return false, errors.New("Not implemented")
+	}
+	return false, errors.New("Unidentified type")
+}
+
+func compareString(comparator *ComparatorCondition, value string) (bool, error) {
+	switch comparator.Operator {
+	case Equal:
+		if comparator.Value == value {
+			return true, nil
+		}
+		return false, nil
+	case Regexp:
+		return regexp.MatchString(comparator.Value, value)
+	}
+	return false, errors.New("Unidentified Comp")
+}
+
+/*
+LogicalCondition
+*/
+type LogicalCondition struct {
+	Operator   string
+	Conditions []Condition
+}
+
+/*
+Add func implements interface, byt adding condition
+*/
+func (comparator *LogicalCondition) Add(condition Condition) {
+	comparator.Conditions = append(comparator.Conditions, condition)
+}
+
+/*
+Remove func implements interface, by Removing condition
+*/
+func (comparator *LogicalCondition) Remove(condition Condition) {
+	comparator.Conditions = append(comparator.Conditions, condition)
+}
+
+/*
+IsMet func implements IsMet
+*/
+func (comparator *LogicalCondition) IsMet(currentEntityJSON *string) (bool, error) {
+	switch comparator.Operator {
+	case Or:
+		for _, condition := range comparator.Conditions {
+			result, err := condition.IsMet(currentEntityJSON)
+			if err != nil {
+				return result, err
+			}
+			if result == true {
+				return true, nil
+			}
+		}
+		return false, nil
+	case And:
+		for _, condition := range comparator.Conditions {
+			result, err := condition.IsMet(currentEntityJSON)
+			if err != nil {
+				return result, err
+			}
+			if result == false {
+				return false, nil
+			}
+		}
+		return true, nil
+	}
+	return false, errors.New("Unidentified operator")
+}
